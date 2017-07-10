4af06c8e84cb4cbe8ca0aac66ae22b1d28704dea
jianjun2015 <jianjun_info@163.com>
2017-07-10 19:38:08 +0800

    BigDecimal 相关

--- com-component-jdbc/src/main/java/component/jdbc/Main.java
+++ com-component-jdbc/src/main/java/component/jdbc/Main.java
@@ -1,7 +1,11 @@
 package component.jdbc;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wangjianjun on 2017/6/20.
@@ -9,15 +13,22 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
+//        testJDBC();
+//        f_function();
+        func_equal();
+    }
 
-        String sql = "select * from country";
+    public static void testJDBC(){
+
+        String sql = "select *  from test";
+
         DBHelper dbHelper = new DBHelper(sql);
         try {
             ResultSet resultSet = dbHelper.pst.executeQuery();
             while (resultSet.next()){
                 int id = resultSet.getInt("id");
-                String countryName = resultSet.getString("countryname");
-                String countryCode = resultSet.getString("countryCode");
+                Double countryName = resultSet.getDouble("balance");
+                String countryCode = resultSet.getString("name");
 
                 System.out.println(id+" "+countryName+" "+" "+countryCode);
             }
@@ -27,6 +38,84 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void f_function() {
+
+        String sql = "select balance+balance as bb from test";
+        sql = "\n" +
+                "SELECT (SELECT case when SUM(settlement_capital) is NULL then 0 else SUM(settlement_capital) end FROM t_amc_fund_detail \n" +
+                "WHERE product_id in (SELECT product_id FROM t_fis_current_honor_order WHERE prod_value_date='2017-07-07')) - (\n" +
+                "SELECT case when sum(investment_amount) is NULL then 0 else sum(investment_amount) end FROM t_fis_current_honor_order \n" +
+                "WHERE product_id in (SELECT product_id FROM t_fis_current_honor_order WHERE prod_value_date='2017-07-07') AND STATUS=3)";
+        DBHelper dbHelper = new DBHelper(sql);
+        try {
+            ResultSet resultSet = dbHelper.pst.executeQuery();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            while (resultSet.next()){
+
+                Map<String, Object> resultRow = new HashMap<>(Math.max(16, (int) (metaData.getColumnCount() / 0.75) + 1));
+                for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                    resultRow.put(metaData.getColumnName(i).toLowerCase(), resultSet.getObject(i));
+                }
+
+
+                Object obj = resultSet.getObject(1);
+                System.out.println(obj);
+                equalZero(resultRow);
+            }
+
+            resultSet.close();
+            dbHelper.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void func_equal(){
+
+        BigDecimal value = BigDecimal.valueOf(0.000000);
+        if (value.compareTo(BigDecimal.ZERO) == 0){
+            System.out.println(true);
+        }
+    }
+
+    private static BigDecimal getValue(Map<String, Object> map){
+
+        if (map == null)
+            return null;
+
+        Object value = map.values().iterator().next();
+
+        if (value instanceof Integer) {
+            System.out.println("Integer");
+            return new BigDecimal((Integer) value);
+        }
+        else if (value instanceof Long) {
+            System.out.println("Long");
+            return new BigDecimal((Long) value);
+        }
+        else if (value instanceof Double) {
+            System.out.println("Double");
+            return new BigDecimal((Double) value);
+        }
+        else if (value instanceof BigDecimal) {
+            System.out.println("BigDecimal");
+            return (BigDecimal) value;
+        }
+        else
+            return BigDecimal.valueOf(-1);
+    }
+
+    private static boolean equalZero(Map<String, Object> map){
+        BigDecimal value = getValue(map);
+        if (value != null && value.compareTo(BigDecimal.ZERO) == 0) {
+            System.out.println(true);
+            return true;
+        }
+
+        System.out.println(false);
+        return false;
     }
 }
---
+++ com-component-util-stringnumbric/src/main/java/component/decimal/BigDecimalC.java
@@ -0,0 +1,55 @@
+package component.decimal;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangjianjun on 2017/7/10.
+ */
+public class BigDecimalC {
+
+    public static void main(String[] args) {
+        func_();
+    }
+
+    public static void func_(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("v1","0.000000");
+        BigDecimal value =  getValue(map);
+        if (value != null && value.compareTo(BigDecimal.ZERO) == 0){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+    }
+
+    private static BigDecimal getValue(Map<String,Object> map){
+        if (map == null || map.isEmpty())
+            return null;
+        Object value = map.values().iterator().next();
+        if (value == null)
+            return null;
+        if (value instanceof Integer)
+            return new BigDecimal((Integer) value);
+        else if (value instanceof Long)
+            return new BigDecimal((Long) value);
+        else if (value instanceof Double)
+            return new BigDecimal((Double) value);
+        else if (value instanceof BigDecimalC)
+            return (BigDecimal) value;
+        else
+            return BigDecimal.valueOf(-1);
+    }
+
+    private static boolean equalZero(Map<String, Object> map){
+        BigDecimal value = getValue(map);
+        if (value != null && value.compareTo(BigDecimal.ZERO) == 0) {
+            System.out.println(true);
+            return true;
+        }
+
+        System.out.println(false);
+        return false;
+    }
+}
