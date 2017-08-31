9c41816aaf86824ce3e1f0ee9996d20e65240bac
jianjun2015 <jianjun_info@163.com>
2017-08-31 11:41:25 +0800

    添加源码测试模块

---
+++ com-src-code-test/src/main/java/datatype/DoubleClass.java
@@ -0,0 +1,7 @@
+package datatype;
+
+/**
+ * Created by wangjianjun on 2017/8/31.
+ */
+public class DoubleClass {
+}
---
+++ com-src-code-test/src/main/java/datatype/IntegerClass.java
@@ -0,0 +1,13 @@
+package datatype;
+
+/**
+ * Created by wangjianjun on 2017/8/31.
+ */
+public class IntegerClass {
+
+    public static void main(String[] args) {
+        Integer integer = 3;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
