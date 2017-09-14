ec7a5adb69bbb848c50aeb026c6d95d292e14d13
jianjun2015 <jianjun_info@163.com>
2017-09-14 09:39:55 +0800

    add

--- com-component-hign-concurrency/src/main/resources/sql/update.sql
+++ com-component-hign-concurrency/src/main/resources/sql/update.sql
@@ -1,1 +1,10 @@
 # drop table if exists tablename
+
+# ALTER TABLE test  MODIFY COLUMN user_age int(4) COMMENT '年龄';
+
+# add data
+# LOCK TABLES `test` WRITE;
+# INSERT INTO `test` VALUES (3,'jack',20,now(),100,0);
+# UNLOCK TABLES;
+
+ALTER TABLE test add COLUMN version_ int(4) DEFAULT 0 COMMENT '版本号-乐观锁机制';
